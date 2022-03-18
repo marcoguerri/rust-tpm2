06df0662e9fd8e7644ae8953a48de79160628bb3
Marco Guerri <marco.guerri.dev@fastmail.com>
2022-03-18 17:00:54 +0100

    Beginning TPM2_Import struct and moving to statically allocated structs

--- src/tpm2/commands/pcr.rs
+++ src/tpm2/commands/pcr.rs
@@ -1,6 +1,5 @@
-use crate::device::raw;
-use crate::device::raw::TpmDeviceOps;
-use crate::device::tcp;
+use crate::device::rawtpm;
+use crate::device::rawtpm::TpmDeviceOps;
 use crate::tpm2::errors;
 use crate::tpm2::serialization::inout;
 use crate::tpm2::serialization::inout::Tpm2StructOut;
@@ -9,39 +8,50 @@ use bytebuffer::ByteBuffer;
 use std::mem;
 use std::result;
 
-// tpm2_pcr_read issues a TPM2_PCR_Read command with TpmsPcrSelection configuration
-// which selects all PCR registers for SHA1 and SHA256 banks.
+// tpm2_pcr_read calls tpm2_pcr_read function returning the content of all
+// PCR Registers in SHA1 and SHA256 form.
 pub fn tpm2_pcr_read() -> result::Result<u32, errors::TpmError> {
+    // build PcrSelection structures, selecting all PCR Registers, for both SHA1 and SHA256
+    let pcr_selections_sha256 = tcg::TpmsPcrSelection {
+        hash: tcg::TPM_ALG_SHA256,
+        sizeof_select: 3,
+        pcr_select: &[0xFF, 0xFF, 0xFF],
+    };
+
+    let pcr_selections_sha1 = tcg::TpmsPcrSelection {
+        hash: tcg::TPM_ALG_SHA1,
+        sizeof_select: 3,
+        pcr_select: &[0xFF, 0xFF, 0xFF],
+    };
+
     let pcr_selection = tcg::TpmlPcrSelection {
         count: 2,
-        pcr_selections: vec![
-            tcg::TpmsPcrSelection {
-                hash: tcg::TPM_ALG_SHA256,
-                sizeof_select: 3,
-                pcr_select: vec![0xFF, 0xFF, 0xFF],
-            },
-            tcg::TpmsPcrSelection {
-                hash: tcg::TPM_ALG_SHA1,
-                sizeof_select: 3,
-                pcr_select: vec![0xFF, 0xFF, 0xFF],
-            },
-        ],
+        pcr_selections: &[pcr_selections_sha1, pcr_selections_sha256],
     };
 
     let mut buffer_pcr_selection = ByteBuffer::new();
     pcr_selection.pack(&mut buffer_pcr_selection);
 
-    let cmd_pcr_read =
-        match super::commands::NewPcrReadCommand(tcg::TPM_ST_NO_SESSION, pcr_selection) {
-            Ok(cmd_pcr_read) => cmd_pcr_read,
-            Err(error) => return Err(error),
+    let pcr_selection_size = buffer_pcr_selection.to_bytes().len();
+    if pcr_selection_size > u32::MAX as usize {
+        errors::TpmError {
+            msg: String::from("pcr_selection size is too big"),
         };
+    }
+
+    let cmd_pcr_read = super::commands::PcrReadCommand {
+        tag: tcg::TPM_ST_NO_SESSION,
+        command_size: super::commands::PCR_READ_PREAMBLE_SIZE + pcr_selection_size as u32,
+        command_code: tcg::TPM_CC_PCR_READ,
+        pcr_selection_in: pcr_selection,
+    };
 
     let mut buffer = ByteBuffer::new();
     inout::pack(&[cmd_pcr_read], &mut buffer);
 
-    let mut tpm_device: raw::TpmDevice = raw::TpmDevice {
-        rw: &mut tcp::TpmSwtpmIO::new(),
+    // write buffer to TPM device and read back response
+    let mut tpm_device: rawtpm::TpmDevice = rawtpm::TpmDevice {
+        rw: &mut rawtpm::TpmRawIO {},
     };
 
     println!(
@@ -54,12 +64,9 @@ pub fn tpm2_pcr_read() -> result::Result<u32, errors::TpmError> {
         Err(err) => println!("error during send_recv: {}", err),
         Ok(_) => println!("answer received correctly!"),
     }
-    let resp = super::commands::NewPcrReadResponse(&mut resp_buffer);
-    match resp {
-        Ok(_) => {
-            println!("{:?}", resp);
-            Ok(0)
-        }
-        Err(err) => Err(err),
-    }
+    println!(
+        "got response buffer as {}",
+        hex::encode(resp_buffer.to_bytes())
+    );
+    Ok(0)
 }
--- src/tpm2/types/tcg.rs
+++ src/tpm2/types/tcg.rs
@@ -1,5 +1,7 @@
 use crate::tpm2::errors;
 use bytebuffer::ByteBuffer;
+use std::convert::TryInto;
+use std::mem;
 use std::result;
 
 use crate::tpm2::serialization::inout;
@@ -10,10 +12,26 @@ pub type TpmCc = u32;
 pub type TpmRc = u32;
 pub type TpmAlgId = u16;
 pub type TpmSu = u16;
+pub type TpmaObject = u32;
+pub type TpmKeyBits = u16;
+
+// Derived types
+// TODO: extend further these types
+
+pub type TpmiAlgPublic = TpmAlgId;
+pub type TpmiAlgHash = TpmAlgId;
+pub type TpmiAlgKdf = TpmAlgId;
+pub type TpmiAlgRsaScheme = TpmAlgId;
+
+pub type TpmiAlgKeyedHashScheme = TpmAlgId;
+
+pub type TpmiRsaKeyBits = TpmKeyBits;
 
 // TPM2 command codes
 pub const TPM_CC_PCR_READ: TpmCc = 0x0000017E;
 pub const TPM_CC_STARTUP: TpmCc = 0x00000144;
+
+pub const HASH_SIZE: usize = 512;
 
 // TPM2 startup types
 pub const TPM_SU_CLEAR: TpmSu = 0x0000;
@@ -24,18 +42,43 @@ pub const TPM_ST_NO_SESSION: TpmiStCommandTag = 0x8001;
 
 // Algorithms
 pub const TPM_ALG_SHA256: TpmAlgId = 0x000B;
-// pub const TPM_ALG_SHA1: TpmAlgId = 0x0004;
+pub const TPM_ALG_KEYEDHASH: TpmAlgId = 0x0008;
+pub const TPM_ALG_SYMCIPHER: TpmAlgId = 0x0025;
+pub const TPM_ALG_RSA: TpmAlgId = 0x0001;
+pub const TPM_ALG_ECC: TpmAlgId = 0x0023;
+
+// TPMU_HA union
+pub union TpmuHa {
+    pub sha1: [u8; 20usize],
+    pub sha256: [u8; 32usize],
+    pub sha384: [u8; 38usize],
+    pub sha512: [u8; 64usize],
+    pub sm3256: [u8; 32usize],
+}
 
 // TPM2B_DIGEST
-#[derive(Default, Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct Tpm2bDigest {
     size: u16,
-    buffer: Vec<u8>,
+    buffer: [u8; mem::size_of::<TpmuHa>()],
 }
 
 impl Tpm2bDigest {
     pub fn get_buffer(&self) -> &[u8] {
         &self.buffer[..]
+    }
+
+    pub fn from_vec(size: u16, buffer: Vec<u8>) -> Self {
+        return Tpm2bDigest {
+            size: size,
+            buffer: buffer.try_into().unwrap_or_else(|v: Vec<u8>| {
+                panic!(
+                    "lenght of array is {}, incoming buffer is {}",
+                    mem::size_of::<TpmuHa>(),
+                    v.len()
+                )
+            }),
+        };
     }
 }
 
@@ -111,7 +154,7 @@ impl inout::Tpm2StructIn for TpmlDigest {
                 _ => (),
             }
             let buffer = buff.read_bytes(size as usize);
-            self.digests.push(Tpm2bDigest { size, buffer });
+            self.digests.push(Tpm2bDigest::from_vec(size, buffer));
         }
         Ok(())
     }
@@ -151,4 +194,184 @@ impl inout::Tpm2StructIn for TpmlPcrSelection {
 
         Ok(())
     }
+}
+
+// TPMU_PUBLIC_PARMS
+#[derive(Copy, Clone)]
+pub union TpmuPublicParms {
+    pub pubkeyedHashDetails: TpmsKeyedHashParms,
+}
+
+// TPMU_PUBLIC_ID
+pub union TpmuPublicId {
+    pub keyedHash: Tpm2bDigest,
+    pub sym: Tpm2bDigest,
+    pub rsa: Tpm2bPublicKeyRsa,
+    pub ecc: TpmsEccPoint,
+    pub derive: TpmsDerive,
+}
+
+// TPMS_SCHEME_HASH
+#[derive(Default, Copy, Clone, Debug)]
+pub struct TpmsSchemeHash {
+    hashAlg: TpmiAlgHash,
+}
+
+pub type TpmsSchemeHmac = TpmsSchemeHash;
+
+pub type TpmsSigSchemeEcdsa = TpmsSchemeHash;
+pub type TpmsSigSchemeRsassa = TpmsSchemeHash;
+pub type TpmsSigSchemeEcdh = TpmsSchemeHash;
+pub type TpmsSigSchemeEcmqv = TpmsSchemeHash;
+pub type TpmsSigSchemeRsapss = TpmsSchemeHash;
+pub type TpmsSigSchemeEcdaa = TpmsSchemeHash;
+pub type TpmsSigSchemeSm2 = TpmsSchemeHash;
+pub type TpmsSigSchemeEcschnorr = TpmsSchemeHash;
+pub type TpmsSigSchemeRsaes = TpmsSchemeHash;
+pub type TpmsSigSchemeOaep = TpmsSchemeHash;
+
+// Types of TPMU_ASYM_SCHEME
+pub union TpmuAsymScheme {
+    pub ecdsa: TpmsSigSchemeEcdsa,
+    pub rsassa: TpmsSigSchemeRsassa,
+    pub ecdh: TpmsSigSchemeEcdh,
+    pub ecmqv: TpmsSigSchemeEcmqv,
+    pub rsapss: TpmsSigSchemeRsapss,
+    pub ecdaa: TpmsSigSchemeEcdaa,
+    pub smc2: TpmsSigSchemeSm2,
+    pub ecschnorr: TpmsSigSchemeEcschnorr,
+    pub rsaes: TpmsSigSchemeRsaes,
+    pub oaep: TpmsSigSchemeOaep,
+}
+
+// TPMS_SCHEME_XOR
+#[derive(Copy, Clone, Debug)]
+pub struct TpmsSchemeXor {
+    hashAlg: TpmiAlgHash,
+    kdf: TpmiAlgKdf,
+}
+
+// TPMU_SCHEME_KEYEDHASH
+#[derive(Copy, Clone, Debug)]
+pub struct TpmuSchemeKeyedHash {
+    hmac: TpmsSchemeHmac,
+    xor: TpmsSchemeXor,
+}
+
+// TPMT_KEYEDHASH_SCHEME
+#[derive(Copy, Clone, Debug)]
+pub struct TpmtKeyedHashScheme {
+    scheme: TpmiAlgKeyedHashScheme,
+    details: TpmuSchemeKeyedHash,
+}
+
+// TPMS_KEYEDHASH_PARMS
+#[derive(Copy, Clone, Debug)]
+pub struct TpmsKeyedHashParms {
+    scheme: TpmtKeyedHashScheme,
+}
+
+// TPMS_SYMCIPHER_PARMS
+#[derive(Default, Debug)]
+pub struct TpmsSymcipherParms {}
+
+// TPMS_ECC_PARMS
+#[derive(Default, Debug)]
+pub struct TpmsEccParms {}
+
+// TPMS_ASYM_PARMS
+#[derive(Default, Debug)]
+pub struct TpmsAsymParms {}
+
+// TPMT_SYM_DEF_OBJECT
+#[derive(Default, Debug)]
+pub struct TpmtSymDefObject {
+    //algorithm: TPMI_ALG_SYM_OBJECT,
+//keyBits: TPMU_SYM_KEY_BITS,
+//mode: TPMU_SYM_MODE,
+//details: TPMU_SYM_DETAILS,
+}
+
+// TPMT_RSA_SCHEME
+pub struct TpmtRsaScheme {
+    scheme: TpmiAlgRsaScheme,
+    details: TpmuAsymScheme,
+}
+
+// TPMS_RSA_PARMS
+#[derive(Debug)]
+pub struct TpmsRsaParams {
+    symmetric: TpmtSymDefObject,
+    scheme: TpmtRsaScheme,
+    keyBits: TpmiRsaKeyBits,
+    exponent: u32,
+}
+
+// TPMS_ECC_PARMS
+#[derive(Default, Debug)]
+pub struct TpmsEccParams {
+    symmetric: TpmtSymDefObject,
+    //scheme: TPMT_ECC_SCHEME,
+    //curveID: TPMI_ECC_CURVE,
+    //kdf: TPMT_KDF_SCHEME,
+}
+
+// TPM2B_LABEL
+#[derive(Copy, Clone, Debug)]
+pub struct Tpm2bLabel {
+    size: u16,
+    buffer: [u8; 512usize],
+}
+
+// TPM2B_ECC_PARAMETER
+#[derive(Copy, Clone, Debug)]
+pub struct Tpm2bEccParameter {
+    size: u16,
+    buffer: [u8; 256usize],
+}
+
+// TPM2B_PUBLIC_KEY_RSA
+#[derive(Copy, Clone, Debug)]
+pub struct Tpm2bPublicKeyRsa {
+    size: u16,
+    buffer: [u8; 512usize],
+}
+
+// TPMS_ECC_POINT
+#[derive(Copy, Clone, Debug)]
+pub struct TpmsEccPoint {
+    x: Tpm2bEccParameter,
+    y: Tpm2bEccParameter,
+}
+
+// TPMS_DERIVE
+#[derive(Copy, Clone, Debug)]
+pub struct TpmsDerive {
+    label: Tpm2bLabel,
+    //context: Tpm2bContext,
+}
+
+// TPMT_PUBLIC
+#[derive(Default, Debug)]
+pub struct TpmtPublic {
+    typeAlg: TpmiAlgPublic,
+    nameAlg: TpmiAlgHash,
+    objectAttributes: TpmaObject,
+    authPolicy: Tpm2bDigest,
+    parameters: TpmuPublicParms,
+    unique: TpmuPublicId,
+}
+
+// TPM2B_PUBLIC
+#[derive(Default, Debug)]
+pub struct Tpm2BPublic {
+    size: u16,
+    public: TpmtPublic,
+}
+
+// TPM2B_DATA
+#[derive(Default, Debug)]
+pub struct Tpm2BData {
+    size: u16,
+    buffer: Vec<u8>,
 }
